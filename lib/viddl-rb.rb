8cfa01e86f00432b63d82a4166fea8810f85fec5
Kalle Lindström <lindstrom.kalle@gmail.com>
2012-07-06 18:10:36 +0200

    More API methods, thread safe stdout redirection
    Some general cleanup and methods to get filenames and url and filename
    hashes. Plugin output redirection is now also thread safe.

--- lib/viddl-rb.rb
+++ lib/viddl-rb.rb
@@ -6,51 +6,64 @@ require "nokogiri"
 require "mechanize"
 require "cgi"
 require "open-uri"
-require "open3"
 require "stringio"
 require "download-helper.rb"
 require "plugin-helper.rb"
 
-Dir[File.join(File.dirname(__FILE__),"../plugins/*.rb")].each { |p| load p }
-
+#require all plugins
+Dir[File.join(File.dirname(__FILE__),"../plugins/*.rb")].each { |p| require p }
 
 module ViddlRb
-
   class PluginError < StandardError; end
 
-  #returns an array of download urls for the specified video url
+  def self.io=(io_object)
+    PluginBase.io = io_object
+  end
+
+  #set the default PluginBase io objec to a StringIO instance.
+  #this will suppress any standard output from the plugins.
+  self.io = StringIO.new
+  
+  #returns an array of hashes containing the download url(s) and filenames(s) 
+  #for the specified video url.
   #if the url does not match any plugin, return nil and if a plugin
-  #throws an error, throw PluginError
-  def self.get_urls(url)
+  #throws an error, throw PluginError.
+  #the reason for returning an array is because some urls will give multiple
+  #download urls (for example a Youtube playlist url).
+  def self.get_urls_and_filenames(url)
     plugin = PluginBase.registered_plugins.find { |p| p.matches_provider?(url) }
 
     if plugin 
       begin
         #we'll end up with an array of hashes with they keys :url and :name
-        #surpress_stdout makes sure that plugins don't print to $stdout
-        download_queue = suppress_stdout { plugin.get_urls_and_filenames(url) }
+        urls_filenames = plugin.get_urls_and_filenames(url)
       rescue StandardError => e
-        message = "Error while running the #{plugin.name.inspect} plugin. Maybe it has to be updated? Error: #{e.message}."
+        message = plugin_error_message(plugin, e.message)
         raise PluginError, message
       end
-      download_queue.map { |url_name| url_name[:url] }
+      urls_filenames
     else
       nil
     end
   end
 
-  #redircts $stdout calls (for example puts) for the given block
-  #to a temporary StringIO object.
-  def self.suppress_stdout
-    std = $stdout
-    $stdout = StringIO.new
-    begin
-      return_value = yield
-    ensure
-      $stdout = std
-      return_value
-    end
+  #returns an array of download urls for the given video url.
+  def self.get_urls(url)
+    urls_filenames = get_urls_and_filenames(url)
+    urls_filenames.nil? ? nil : urls_filenames.map { |uf| uf[:url] }
   end
-  private_class_method :suppress_stdout
 
+  #returns an array of filenames for the given video url.
+  def self.get_filenames(url)
+    urls_filenames = get_urls_and_filenames(url)
+    urls_filenames.nil? ? nil : urls_filenames.map { |uf| uf[:name] }
+  end
+
+  #<<< helper methods >>>
+
+  #the default error message when a plugin fails to download a video.
+  def self.plugin_error_message(plugin, error)
+    "Error while running the #{plugin.name.inspect} plugin. Maybe it has to be updated? Error: #{error}."
+  end
+  private_class_method :plugin_error_message
 end
