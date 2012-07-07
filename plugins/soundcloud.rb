150e388dac350c6827296f51739c4408fec640bd
Kalle Lindström <lindstrom.kalle@gmail.com>
2012-07-07 11:58:43 +0200

    Wrapped in ViddlRb module

--- plugins/soundcloud.rb
+++ plugins/soundcloud.rb
@@ -1,38 +1,39 @@
-class Soundcloud < PluginBase
-  # this will be called by the main app to check whether this plugin is responsible for the url passed
-  def self.matches_provider?(url)
-    url.include?("soundcloud.com")
+module ViddlRb
+  class Soundcloud < PluginBase
+    # this will be called by the main app to check whether this plugin is responsible for the url passed
+    def self.matches_provider?(url)
+      url.include?("soundcloud.com")
+    end
+
+    # return the url for original video file and title
+    def self.get_urls_and_filenames(url)
+      doc          = Nokogiri::XML(open(url))
+      download_filename = doc.at("#main-content-inner img[class=waveform]").attributes["src"].value.to_s.match(/\.com\/(.+)\_/)[1]
+      download_url = "http://media.soundcloud.com/stream/#{download_filename}"
+      file_name    = transliterate("#{doc.at('//h1/em').text.chomp}") + ".mp3"
+
+      [{:url => download_url, :name => file_name}]
+    end
+
+    def self.transliterate(str)
+      # Based on permalink_fu by Rick Olsen
+
+      # Downcase string
+      str.downcase!
+
+      # Remove apostrophes so isn't changes to isnt
+      str.gsub!(/'/, '')
+
+      # Replace any non-letter or non-number character with a space
+      str.gsub!(/[^A-Za-z0-9]+/, ' ')
+
+      # Remove spaces from beginning and end of string
+      str.strip!
+
+      # Replace groups of spaces with single hyphen
+      str.gsub!(/\ +/, '-')
+
+      str
+    end
   end
-
-  # return the url for original video file and title
-  def self.get_urls_and_filenames(url)
-    doc          = Nokogiri::XML(open(url))
-    download_filename = doc.at("#main-content-inner img[class=waveform]").attributes["src"].value.to_s.match(/\.com\/(.+)\_/)[1]
-    download_url = "http://media.soundcloud.com/stream/#{download_filename}"
-    file_name    = transliterate("#{doc.at('//h1/em').text.chomp}") + ".mp3"
-
-    [{:url => download_url, :name => file_name}]
-  end
-
-  def self.transliterate(str)
-  # Based on permalink_fu by Rick Olsen
-
-  # Downcase string
-  str.downcase!
-
-  # Remove apostrophes so isn't changes to isnt
-  str.gsub!(/'/, '')
-
-  # Replace any non-letter or non-number character with a space
-  str.gsub!(/[^A-Za-z0-9]+/, ' ')
-
-  # Remove spaces from beginning and end of string
-  str.strip!
-
-  # Replace groups of spaces with single hyphen
-  str.gsub!(/\ +/, '-')
-
-  str
-end
-
 end
--- plugins/vimeo.rb
+++ plugins/vimeo.rb
@@ -1,34 +1,34 @@
+module ViddlRb
+  class Vimeo < PluginBase
+    #this will be called by the main app to check whether this plugin is responsible for the url passed
+    def self.matches_provider?(url)
+      url.include?("vimeo.com")
+    end
+    
+    def self.get_urls_and_filenames(url)
+      #the vimeo ID consists of 7 decimal numbers in the URL
+      vimeo_id = url[/\d{7,8}/]
 
+      agent = Mechanize.new      #use Mechanize for the automatic cookie handeling
+      agent.redirect_ok = false  #don't follow redirects so we do not download the video when we get it's url
 
-class Vimeo < PluginBase
-  #this will be called by the main app to check whether this plugin is responsible for the url passed
-  def self.matches_provider?(url)
-    url.include?("vimeo.com")
-  end
-  
-  def self.get_urls_and_filenames(url)
-    #the vimeo ID consists of 7 decimal numbers in the URL
-    vimeo_id = url[/\d{7,8}/]
+      video_page = agent.get("http://vimeo.com/#{vimeo_id}")
+      page_html = video_page.root.inner_html
 
-    agent = Mechanize.new      #use Mechanize for the automatic cookie handeling
-    agent.redirect_ok = false  #don't follow redirects so we do not download the video when we get it's url
+      title = page_html[/<meta\s+property="og:title"\s+content="(.+?)"/, 1]
+      puts "[VIMEO] Title: #{title}"
 
-    video_page = agent.get("http://vimeo.com/#{vimeo_id}")
-    page_html = video_page.root.inner_html
+      #the timestamp and sig info is in the embedded player javascript in the video page
+      timestamp = page_html[/"timestamp":(\d+),/, 1]
+      signature = page_html[/"signature":"([\d\w]+)",/, 1]
 
-    title = page_html[/<meta\s+property="og:title"\s+content="(.+?)"/, 1]
-    puts "[VIMEO] Title: #{title}"
+      redirect_url = "http://player.vimeo.com/play_redirect?clip_id=#{vimeo_id}&sig=#{signature}&time=#{timestamp}&quality=hd,sd&codecs=H264,VP8,VP6"
 
-    #the timestamp and sig info is in the embedded player javascript in the video page
-    timestamp = page_html[/"timestamp":(\d+),/, 1]
-    signature = page_html[/"signature":"([\d\w]+)",/, 1]
+      #the download url is the value of the location (redirect) header
+      download_url = agent.get(redirect_url).header["location"]
+      file_name = PluginBase.make_filename_safe(title) + ".mp4"
 
-    redirect_url = "http://player.vimeo.com/play_redirect?clip_id=#{vimeo_id}&sig=#{signature}&time=#{timestamp}&quality=hd,sd&codecs=H264,VP8,VP6"
-
-    #the download url is the value of the location (redirect) header
-    download_url = agent.get(redirect_url).header["location"]
-    file_name = PluginBase.make_filename_safe(title) + ".mp4"
-
-    [{:url => download_url, :name => file_name}]
+      [{:url => download_url, :name => file_name}]
+    end
   end
 end
